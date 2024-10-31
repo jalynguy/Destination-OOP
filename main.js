fe9039b72ea2932d8e7cef53d383f0ca4594b513
James Nguyen <james.nguyen@james.nguyen-MacBook-Pro>
2024-10-31 10:57:32 -0700

    Adding class imports

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // Import classes here
-
+const { Airport } = require('./classes/Airport');
+const { Bag } = require('./classes/Bag');
+const { Person } = require('./classes/Person');
+const { Plane } = require('./classes/Plane');
 
 function main() {
     // You can run any of your classes here to test the outputs
